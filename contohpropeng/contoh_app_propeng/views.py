895559134e60eaddf6298062a939fcc3db164c06
bil <118203904+bangABB@users.noreply.github.com>
2025-02-14 12:25:51 +0700

    Initial commit buat propensi, nanti bakal dihapus aja projects+webapp sebelum fix nama aplikasi

--- contohpropeng/contoh_app_propeng/views.py
+++ contohpropeng/contoh_app_propeng/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, misel ica dien arshad!")
+    return HttpResponse("Hello, Misel Ica Dien Arshad!")
