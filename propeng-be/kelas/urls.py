cb4a3c01a86adba847a26f78b60d622902c72143
Muhammad Fawwaz Arshad Said <muhammad.fawwaz22@ui.ac.id>
2025-03-08 14:11:38 +0700

    feat: edit urls kelas

--- propeng-be/kelas/urls.py
+++ propeng-be/kelas/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from kelas.views import list_kelas, create_kelas, detail_kelas, delete_kelas, update_kelas
 
 urlpatterns = [
+    path('', list_kelas, name='list_kelas'),
     path('create/', create_kelas, name='create_kelas'),
     path('<int:kelas_id>/', detail_kelas, name='detail_kelas'),
     path('delete/<int:kelas_id>', delete_kelas, name='delete_kelas'),
