5eb3cd8521beb56bf7d28038a1a1c51fd57115a6
Muhammad Fawwaz Arshad Said <muhammad.fawwaz22@ui.ac.id>
2025-05-17 21:40:23 +0700

    Update Django model fields with choices

--- propeng-be/evalguru/models.py
+++ propeng-be/evalguru/models.py
@@ -6,25 +6,25 @@ from matapelajaran.models import MataPelajaran
 # Ini gua gatau sih staticnya gimana jadi gua nge-state 1-5
 class EvalGuru(models.Model):
     pilihanvariabel = [
-        1, 2, 3, 4, 5
+        (1,1), (2,2), (3,3), (4,4), (5,5)
     ]
     pilihanindikator = [
-        1, 2, 3, 4, 5
+        (1,1), (2,2), (3,3), (4,4), (5,5)
     ]
     pilihanskorlikert = [
-        1, 2, 3, 4, 5
+        (1,1), (2,2), (3,3), (4,4), (5,5)
     ]
     guru = models.ForeignKey(
         Teacher,
-        related_name='guru',
+        related_name='evalguru_guru',
         on_delete=models.CASCADE)
     matapelajaran = models.ForeignKey(
         MataPelajaran,
-        related_name='matapelajaran',
+        related_name='evalguru_matapelajaran',
         on_delete=models.CASCADE)
     siswa = models.ForeignKey(
         Student,
-        related_name='siswa',
+        related_name='evalguru_siswa',
         on_delete=models.CASCADE)
     variabel = models.PositiveIntegerField(choices=pilihanvariabel)
     indikator = models.PositiveIntegerField(choices=pilihanindikator)
