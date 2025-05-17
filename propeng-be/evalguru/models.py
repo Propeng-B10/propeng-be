4b2d9370bf2275e3afee8716333c25680da3c9a5
Muhammad Fawwaz Arshad Said <muhammad.fawwaz22@ui.ac.id>
2025-05-17 21:33:43 +0700

    Add evaluation model for teachers and students

--- propeng-be/evalguru/models.py
+++ propeng-be/evalguru/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from user.models import Teacher, Student 
+from matapelajaran.models import MataPelajaran
 
-# Create your models here.
+
+class EvalGuru(models.Model):
+    pilihanvariabel = [
+        1, 2, 3, 4, 5
+    ]
+    pilihanindikator = [
+        1, 2, 3, 4, 5
+    ]
+    pilihanskorlikert = [
+        1, 2, 3, 4, 5
+    ]
+    guru = models.ForeignKey(
+        Teacher,
+        related_name='guru',
+        on_delete=models.CASCADE)
+    matapelajaran = models.ForeignKey(
+        MataPelajaran,
+        related_name='matapelajaran',
+        on_delete=models.CASCADE)
+    siswa = models.ForeignKey(
+        Student,
+        related_name='siswa',
+        on_delete=models.CASCADE)
+    variabel = models.PositiveIntegerField(choices=pilihanvariabel)
+    indikator = models.PositiveIntegerField(choices=pilihanindikator)
+    skorlikert = models.PositiveIntegerField(choices=pilihanskorlikert)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
